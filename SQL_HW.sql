0dfeac90f64044782bad51a317e1c8def761a2da
logan7734 <logan7734@gmail.com>
2019-04-13 07:12:27 -0500

    finishing up hw

--- SQL_HW.sql
+++ SQL_HW.sql
@@ -1,6 +1,6 @@
 USE sakila;
 
-#1a
+#1a	
 SELECT first_name, last_name FROM actor;
 
 #1b
@@ -116,3 +116,115 @@ INNER JOIN customer c
 ON p.customer_id = c.customer_id
 GROUP BY p.customer_id
 ORDER BY last_name ASC;
+
+    
+#7a. 
+
+SELECT title FROM film
+WHERE language_id in
+	(SELECT language_id 
+	FROM language
+	WHERE name = "English" )
+AND (title LIKE "K%") OR (title LIKE "Q%");
+
+ #7b.
+
+SELECT last_name, first_name
+FROM actor
+WHERE actor_id in
+	(SELECT actor_id FROM film_actor
+	WHERE film_id in 
+		(SELECT film_id FROM film
+		WHERE title = "Alone Trip"));
+        
+#7c.
+
+SELECT country, last_name, first_name, email
+FROM country c
+LEFT JOIN customer cu
+ON c.country_id = cu.customer_id
+WHERE country = 'Canada';
+
+#7d.
+
+SELECT title, category
+FROM film_list
+WHERE category = 'Family';
+		
+
+#7e
+
+SELECT i.film_id, f.title, COUNT(r.inventory_id) AS Inventory
+FROM inventory i
+INNER JOIN rental r
+ON i.inventory_id = r.inventory_id
+INNER JOIN film_text f 
+ON i.film_id = f.film_id
+GROUP BY r.inventory_id
+ORDER BY COUNT(r.inventory_id) DESC;
+
+#7f
+
+SELECT store.store_id, SUM(amount)
+FROM store
+INNER JOIN staff
+ON store.store_id = staff.store_id
+INNER JOIN payment p 
+ON p.staff_id = staff.staff_id
+GROUP BY store.store_id
+ORDER BY SUM(amount);
+
+#7g
+
+SELECT s.store_id, city, country
+FROM store s
+INNER JOIN customer cu
+ON s.store_id = cu.store_id
+INNER JOIN staff st
+ON s.store_id = st.store_id
+INNER JOIN address a
+ON cu.address_id = a.address_id
+INNER JOIN city ci
+ON a.city_id = ci.city_id
+INNER JOIN country coun
+ON ci.country_id = coun.country_id;
+
+
+#7h
+
+SELECT c.name AS 'Genre', SUM(p.amount) AS 'Gross' 
+FROM category c
+JOIN film_category fc 
+ON (c.category_id=fc.category_id)
+JOIN inventory i 
+ON (fc.film_id=i.film_id)
+JOIN rental r 
+ON (i.inventory_id=r.inventory_id)
+JOIN payment p 
+ON (r.rental_id=p.rental_id)
+GROUP BY c.name ORDER BY Gross  LIMIT 5;
+
+
+#8a
+
+USE Sakila;
+
+CREATE VIEW top_five_grossing_genres AS
+
+SELECT c.name AS 'Genre', SUM(p.amount) AS 'Gross' 
+FROM category c
+JOIN film_category fc 
+ON (c.category_id=fc.category_id)
+JOIN inventory i 
+ON (fc.film_id=i.film_id)
+JOIN rental r 
+ON (i.inventory_id=r.inventory_id)
+JOIN payment p 
+ON (r.rental_id=p.rental_id)
+GROUP BY c.name ORDER BY Gross  LIMIT 5;
+
+#8b
+SELECT * FROM top_five_grossing_genres;
+
+#8c
+DROP VIEW top_five_grossing_genres;
